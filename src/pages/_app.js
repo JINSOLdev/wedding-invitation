3a07f57107bfafbf789b7631145b59cd876f665c
Jinsol Kim <jinsol032360@gmail.com>
2024-02-23 16:38:33 +0900

    fix : viewport meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,5 @@
 import '@/styles/globals.css';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <meta name="viewport" content="width=device-width" />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
