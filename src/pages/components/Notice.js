6afed3477dc32122c86e4a3fb08be07cef46b85c
Jinsol Kim <jinsol032360@gmail.com>
2024-02-26 17:10:27 +0900

    add : period dot

--- src/pages/components/Notice.js
+++ src/pages/components/Notice.js
@@ -3,9 +3,11 @@ export default function Notice() {
         <div className="notice">
             <div className="notice-text">
                 <p style={{ fontWeight: 'bold' }}>** 안내 **</p>
-                <p><strong>성당 규정상 화환은 정중히 사양합니다</strong></p>
+                <p>
+                    <strong>성당 규정상 화환은 정중히 사양합니다.</strong>
+                </p>
                 <p>화환 대신 축복미를 보내주시면</p>
-                <p>어려운 이웃에게 전달하겠습니다</p>
+                <p>어려운 이웃에게 전달하겠습니다.</p>
                 <p>(축복미 번호 : 02-727-2254)</p>
             </div>
         </div>
