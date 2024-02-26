e58eb29a0a5f20a94dd83addd69af744116c2d55
Jinsol Kim <jinsol032360@gmail.com>
2024-02-26 16:54:19 +0900

    fix : main-text font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,9 @@ export default function Document() {
                     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
                     crossOrigin="anonymous"
                 ></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Nanum+Brush+Script&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
