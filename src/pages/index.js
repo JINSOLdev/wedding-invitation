f02af90320d619ccd985f9689627b7a57333fc0a
Jinsol Kim <jinsol032360@gmail.com>
2024-02-28 14:42:43 +0900

    fix : 카카오내비 주석처리

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
                 <Greeting />
                 <Calendar />
                 <Map />
-                <Navigation />
+                {/* <Navigation /> */}
                 <Transport />
                 <Contact />
                 <Account />
