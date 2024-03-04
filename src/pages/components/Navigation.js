ceb3a1567139078e56a0f7af2394015a8e64c7ac
Jinsol Kim <jinsol032360@gmail.com>
2024-03-04 10:24:58 +0900

    add : map

--- src/pages/components/Navigation.js
+++ src/pages/components/Navigation.js
@@ -4,18 +4,18 @@ export default function Navigation() {
     return (
         <div className="navi-group">
             <div className="navi-naver">
-                <a href="http://naver.me/GvkXFE6S">
-                    <img alt="naver-map" className="map-icon" id="naver-map" src="img/map/naver-map.png" height="60" width="60" />
+                <a href="https://naver.me/FZWifjM8">
+                    <img alt="naver-map" className="map-icon" id="naver-map" src="img/map/naver-map.png" height="70" width="70" />
                 </a>
             </div>
             <div className="navi-kakao">
-                <a href="https://map.kakao.com/?itemId=26633765">
-                    <img alt="kakao-map" className="map-icon" id="kakao-map" src="img/map/kakao-map.png" height="60" width="60" />
+                <a href="https://kko.to/nPvPinmwNy">
+                    <img alt="kakao-map" className="map-icon" id="kakao-map" src="public/kakao-map.jpg" height="70" width="70" />
                 </a>
             </div>
             <div className="navi-google">
-                <a href="https://maps.app.goo.gl/z59tx6AuZZ48PhWVA">
-                    <img alt="google-map" className="map-icon" id="google-map" src="img/map/google-map.png" height="60" width="60" />
+                <a href="https://maps.app.goo.gl/m4VQWU1C8PZ3bHix8">
+                    <img alt="google-map" className="map-icon" id="google-map" src="img/map/google-map.png" height="70" width="70" />
                 </a>
             </div>
         </div>
